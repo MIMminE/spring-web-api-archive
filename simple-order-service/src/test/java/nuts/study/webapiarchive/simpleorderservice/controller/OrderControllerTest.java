5c00aedacfa20d56f7a6035bbf1d8f53341cb722
MIMminE <weluv.ww@gmail.com>
2025-11-25 15:07:41 +0900

    feat: CI PR 테스트

--- simple-order-service/src/test/java/nuts/study/webapiarchive/simpleorderservice/controller/OrderControllerTest.java
+++ simple-order-service/src/test/java/nuts/study/webapiarchive/simpleorderservice/controller/OrderControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
@@ -27,7 +27,7 @@ public class OrderControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockitoBean
+    @MockitoSpyBean
     private OrderService orderService;
 
     @Test
