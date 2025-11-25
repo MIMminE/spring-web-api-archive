170158ff5990fd829059179f73f69b293d3fe7c9
MIMminE <weluv.ww@gmail.com>
2025-11-25 15:48:07 +0900

    feat: inventory 서비스 프로젝트 추가

---
+++ simple-inventory-service/src/main/java/nuts/study/webapiarchive/simpleinventoryservice/SimpleInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package nuts.study.webapiarchive.simpleinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleInventoryServiceApplication.class, args);
+    }
+
+}
